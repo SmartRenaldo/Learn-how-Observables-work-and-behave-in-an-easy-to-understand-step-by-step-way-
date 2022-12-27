21024dc1df3f49b44f0371bceac0e9a7fa3c0f7f
SmartRenaldo <correctgavin@gmail.com>
2022-12-27 20:11:09 +1030

    add folder 18 Combine Latest

---
+++ 18 Combine Latest/src/index.ts
@@ -0,0 +1,24 @@
+import { combineLatest, fromEvent } from 'rxjs';
+
+const temperatureInput = document.getElementById('temperature-input');
+const conversionDropdown = document.getElementById('conversion-dropdown');
+const resultText = document.getElementById('result-text');
+
+const temperatureInputEvent$ = fromEvent(temperatureInput, 'input');
+const conversionInputEvent$ = fromEvent(conversionDropdown, 'input');
+
+combineLatest([temperatureInputEvent$, conversionInputEvent$]).subscribe(
+  ([temperatureInputEvent, conversionInputEvent]) => {
+    const temperature = Number(temperatureInputEvent.target['value']);
+    const conversion = conversionInputEvent.target['value'];
+
+    let result: number;
+    if (conversion === 'f-to-c') {
+      result = ((temperature - 32) * 5) / 9;
+    } else if (conversion === 'c-to-f') {
+      result = (temperature * 9) / 5 + 32;
+    }
+
+    resultText.innerText = String(result);
+  }
+);
